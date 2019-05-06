2b2dfbf8bc7298a408387947beee7b99c231bd02
Alisa Ono <alisao.mit@gmail.com>
2019-05-05 23:15:22 -0400

    Deployment bug (4)

--- models/utils.js
+++ models/utils.js
@@ -1,10 +1,10 @@
 module.exports = function(models){
-  const User = models.user;
-  const Class = models.class;
-  const Section = models.section;
-  const FileSystemObject = models.file_system_object;
-  const GradingSystems = models.grading_system;
-  const Source = models.source;
+  const User = models.User;
+  const Class = models.Class;
+  const Section = models.Section;
+  const FileSystemObject = models.FileSystemObject;
+  const GradingSystems = models.GradingSystem;
+  const Source = models.Source;
 
   return {
     createClass: function (name, userId){
@@ -12,10 +12,10 @@ module.exports = function(models){
         class_name: name
       })
       //create the global section
-      .then((nb_class) => 
+      .then((nb_class) =>
         Section.create({section_name: 'global', is_global: true, class_id: nb_class.id})
         .then((section) => nb_class.setGlobalSection(section))
-        .then(() => User.findByPk(userId).then((user) => 
+        .then(() => User.findByPk(userId).then((user) =>
           Promise.all([
             nb_class.setCreator(user),
             nb_class.setInstructors(user),
@@ -24,7 +24,7 @@ module.exports = function(models){
         .then(() => FileSystemObject.create({filename: name, is_directory: true})
           .then((dir) => dir.setClass(nb_class)))
         )
-        .then(() => 
+        .then(() =>
           GradingSystems.create({
             grading_system_name: "default",
             class_id: nb_class.id,
@@ -55,32 +55,32 @@ module.exports = function(models){
               }
             ]
           }, {include: [{association: 'GradingThresholds'}, {association: 'Criteria'}]})
-          
+
         )
       .then(() => nb_class));
     },
 
     addStudent: function(classId, userId){
       return Class.findByPk(classId, {include:[{association: 'GlobalSection'}]})
-      .then((nb_class) => 
-        User.findByPk(userId).then((user) => 
-          nb_class.GlobalSection.addMemberStudent(user)   
+      .then((nb_class) =>
+        User.findByPk(userId).then((user) =>
+          nb_class.GlobalSection.addMemberStudent(user)
         )
       );
     },
 
     createFile: function(parentId, filename, filepath){
       return FileSystemObject.findByPk(parentId, {include:[{association: 'Class'}]})
-      .then((folder) => 
+      .then((folder) =>
         FileSystemObject.create({
-          filename: filename, 
+          filename: filename,
           is_directory: false,
-          parent_id: folder.id, 
+          parent_id: folder.id,
           Source:{filepath: filepath, filename: filename, class_id: folder.Class.id}
         },
           {include: {model: Source, as: 'Source'}
         })
-          // .then((child) => 
+          // .then((child) =>
           //   Promise.all([
           //     child.setParent(folder),
           //   ])
@@ -89,4 +89,3 @@ module.exports = function(models){
     }
   };
 };
-
