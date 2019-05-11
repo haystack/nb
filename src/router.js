7b77a6208b39244ab1efc313f7000c66fe58a72d
Alisa Ono <alisao.mit@gmail.com>
2019-05-11 16:29:14 -0400

    Fix typo

--- src/router.js
+++ src/router.js
@@ -14,7 +14,7 @@ export default new Router({
       path: '/home',
       name: 'home-page',
       props: { default: true },
-      component: () => import('./views/Homepage.vue'),
+      component: () => import('./views/HomePage.vue'),
     },
     // {
     //   path: '/grading',
