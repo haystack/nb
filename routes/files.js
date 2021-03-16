9c56702bdd39a1c0cc5d20d5f4e233726ecf0734
Helen Li <helen82198@gmail.com>
2021-03-16 12:13:29 -0700

    Create place to upload PDF files on the server and use a nb_viewer.html to load pdf into pdf.js viewer

--- routes/files.js
+++ routes/files.js
@@ -3,6 +3,8 @@ const FileSystemObject = require('../models').FileSystemObject;
 const Assignment = require('../models').Assignment;
 const router = express.Router();
 const utils = require('../models/utils')(require('../models'));
+const multer  = require('multer');
+var upload = multer({ dest: 'public/uploads/' });
 
 /**
  * Gets root level file of class
@@ -75,6 +77,25 @@ router.post('/file/:id', (req, res) => {
   
   
 });
+
+/**
+ * Creates child file of current directory
+ * @name POST/api/files/filePdf/:id
+ * @param id: id of parent directory
+ * @param name: name of child file
+ * @param url: url of child file, if html
+ */
+
+router.post('/filePdf/:id', upload.single("file"),(req, res) => {
+  let url = req.headers.origin + "/nb_viewer.html?id=" + req.file.path.substring(15) // only get the id of the file without any paths 
+  console.log(url)
+  utils.createFile(req.params.id, req.body.name, url).then((child) => {
+    if(!child) console.log("NO CHILD!")
+    console.log(child)
+    res.status(200).json(child);
+  })
+});
+
 
 /**
  * Mark file as deleted
