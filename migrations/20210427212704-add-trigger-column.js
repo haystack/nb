81c70175465b3e9e515ccf2ac8fc8cffea37b9ac
Helen Li <helen82198@gmail.com>
2021-04-25 11:02:36 -0700

    Add migration file for trigger type

---
+++ migrations/20210427212704-add-trigger-column.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.addColumn('spotlight_logs', 'trigger', {
+            type: Sequelize.ENUM,
+            values: ['NONE', 'INSTRUCTOR_COMMENTED', 'REPLY_REQUESTED', 'USER_TAGGED', 'USER_SAW_RECENT_ACTIVITY']
+        });
+        
+    },
+    
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.removeColumn('spotlight_logs', 'trigger');
+        await queryInterface.sequelize.query('DROP TYPE IF EXISTS "enum_spotlight_logs_trigger";');
+    }
+};
