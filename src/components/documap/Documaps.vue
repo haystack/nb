f6005c6ade6afae30ab276e7f0f9d9b0bde8be71
Jumana <jumana.almahmoud@gmail.com>
2021-12-08 00:54:59 -0500

    add deleted files

---
+++ src/components/documap/Documaps.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="documaps">
+        <documap
+            v-for="reading in readings"
+            :key="reading.id"
+            :reading="reading"
+            :settings="settings">
+        </documap>
+    </div>
+</template>
+
+<script>
+import Documap from './Documap.vue'
+
+export default {
+    name: 'documaps',
+    props: {
+        readings: {
+            type: Array,
+            default: () => []
+        },
+        settings: {
+            type: Object,
+            default: () => {}
+        },
+    },
+    methods: {
+    },
+    components: {
+        Documap,
+    }  
+}
+</script>
+
+<style>
+.documaps {
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    align-content: space-around;
+}
+</style>
