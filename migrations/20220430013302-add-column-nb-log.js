098d05e9bad01c80cd128d42918cd9dc77502782
Jumana Almahmoud <jumana.almahmoud@gmail.com>
2022-04-30 16:49:50 -0400

    fix query to add event type

--- migrations/20220430013302-add-column-nb-log.js
+++ migrations/20220430013302-add-column-nb-log.js
@@ -2,10 +2,7 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.changeColumn('nb_logs', 'event', 
-      {type: Sequelize.ENUM,
-      values: ['CLICK', 'HOVER', 'STAR', 'REPLY_REQUEST', 'BOOKMARK', 'REPLY', 'SESSION_START', 'SESSION_END', 'HIDE_HIGHLIGHT', 'SHOW_HIGHLIGHT', 'NOTIFICATION_ON', 'NOTIFICATION_OFF', 'NEW_ANNOTATION', 'SYNC_RECEIVED_ANNOTATION', 'SYNC_RECEIVED_REPLY', 'SYNC_RECEIVED_CONNECTION', 'SCROLL', 'FILTER', 'SORT', 'SLIDER_CHANGE']}
-    )
+    await queryInterface.sequelize.query("ALTER TYPE enum_nb_logs_event ADD VALUE 'SLIDER_CHANGE'")
     await queryInterface.addColumn('nb_logs', 'is_comment_endorsed', {
       type:  Sequelize.BOOLEAN,
       allowNull: false,
@@ -24,10 +21,7 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.changeColumn('nb_logs', 'event', 
-    {type: Sequelize.ENUM,
-    values: ['CLICK', 'HOVER', 'STAR', 'REPLY_REQUEST', 'BOOKMARK', 'REPLY', 'SESSION_START', 'SESSION_END', 'HIDE_HIGHLIGHT', 'SHOW_HIGHLIGHT', 'NOTIFICATION_ON', 'NOTIFICATION_OFF', 'NEW_ANNOTATION', 'SYNC_RECEIVED_ANNOTATION', 'SYNC_RECEIVED_REPLY', 'SYNC_RECEIVED_CONNECTION', 'SCROLL', 'FILTER', 'SORT']}
-  )
+    await queryInterface.sequelize.query("ALTER TYPE enum_nb_logs_event DROP VALUE 'SLIDER_CHANGE'")
     await queryInterface.removeColumn('nb_logs', 'is_comment_endorsed');
     await queryInterface.removeColumn('nb_logs', 'is_comment_followed');
     await queryInterface.removeColumn('nb_logs', 'slider_value');
