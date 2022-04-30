3d8774e9e3679200fff507d734836a5b3364feeb
Jumana Almahmoud <jumana.almahmoud@gmail.com>
2022-04-30 16:45:13 -0400

    fix table name

--- migrations/20220430013302-add-column-nb-log.js
+++ migrations/20220430013302-add-column-nb-log.js
@@ -2,21 +2,21 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.changeColumn('nb_log', 'event', 
+    await queryInterface.changeColumn('nb_logs', 'event', 
       {type: Sequelize.ENUM,
       values: ['CLICK', 'HOVER', 'STAR', 'REPLY_REQUEST', 'BOOKMARK', 'REPLY', 'SESSION_START', 'SESSION_END', 'HIDE_HIGHLIGHT', 'SHOW_HIGHLIGHT', 'NOTIFICATION_ON', 'NOTIFICATION_OFF', 'NEW_ANNOTATION', 'SYNC_RECEIVED_ANNOTATION', 'SYNC_RECEIVED_REPLY', 'SYNC_RECEIVED_CONNECTION', 'SCROLL', 'FILTER', 'SORT', 'SLIDER_CHANGE']}
     )
-    await queryInterface.addColumn('nb_log', 'is_comment_endorsed', {
+    await queryInterface.addColumn('nb_logs', 'is_comment_endorsed', {
       type:  Sequelize.BOOLEAN,
       allowNull: false,
       defaultValue: false
   });
-  await queryInterface.addColumn('nb_log', 'is_comment_followed', {
+  await queryInterface.addColumn('nb_logs', 'is_comment_followed', {
     type:  Sequelize.BOOLEAN,
     allowNull: false,
     defaultValue: false
   });
-  await queryInterface.addColumn('nb_log', 'slider_value', {
+  await queryInterface.addColumn('nb_logs', 'slider_value', {
     type:  Sequelize.INTEGER,
     allowNull: false,
     defaultValue: 0.
@@ -24,12 +24,12 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.changeColumn('nb_log', 'event', 
+    await queryInterface.changeColumn('nb_logs', 'event', 
     {type: Sequelize.ENUM,
     values: ['CLICK', 'HOVER', 'STAR', 'REPLY_REQUEST', 'BOOKMARK', 'REPLY', 'SESSION_START', 'SESSION_END', 'HIDE_HIGHLIGHT', 'SHOW_HIGHLIGHT', 'NOTIFICATION_ON', 'NOTIFICATION_OFF', 'NEW_ANNOTATION', 'SYNC_RECEIVED_ANNOTATION', 'SYNC_RECEIVED_REPLY', 'SYNC_RECEIVED_CONNECTION', 'SCROLL', 'FILTER', 'SORT']}
   )
-    await queryInterface.removeColumn('nb_log', 'is_comment_endorsed');
-    await queryInterface.removeColumn('nb_log', 'is_comment_followed');
-    await queryInterface.removeColumn('nb_log', 'slider_value');
+    await queryInterface.removeColumn('nb_logs', 'is_comment_endorsed');
+    await queryInterface.removeColumn('nb_logs', 'is_comment_followed');
+    await queryInterface.removeColumn('nb_logs', 'slider_value');
   }
 };
